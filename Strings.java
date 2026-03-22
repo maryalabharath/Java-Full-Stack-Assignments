697b992f67772a5691d0d40d67cd425c3b2e1e46
Prem <kandulapremchandyadav@gmail.com>
2026-03-22 12:01:01 +0530

    added strings

---
+++ Strings.java
@@ -0,0 +1,10 @@
+public class Strings{
+	public static void main(String []args) {
+String s1 = "Prem";
+String s2 = "CHAND";
+	System.out.println(s1+s2);
+	System.out.println((s1+s2).toLowerCase());
+	System.out.println((s1+s2).toUpperCase());
+	System.out.println("Length of the concatinated string is: "+(s1+s2).length());
+	}
+}
