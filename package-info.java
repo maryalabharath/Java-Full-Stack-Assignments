61850d2d59a8508a9a87ca31302e2d6eb0874456
Shiva3616 <shivakummari3616@gmail.com>
2026-03-24 21:48:25 +0530

    java progrmas

--- package-info.java
+++ package-info.java
@@ -1,7 +1,42 @@
+package bankManagementSystem;
 
-package javapractice;
-class HelloWorld {
+class BankAccountSystem {
+	private float bankBalance;
+	
+	String name;
+	String accType;
+	int age;
+	
+	void setbankBalance() {
+		bankBalance = 100;
+	}
+	
+	
+	BankAccountSystem(String name, String accType, int age) {
+		this.name = name;
+		this.accType = accType;
+		this.age = age;	
+	}
+	
+	void deposite(float amount) {
+		bankBalance = bankBalance + amount;
+		System.out.println("deposited Successfully!");
+	}
+	
+	void withdraw(float amount) {
+		if(amount >= bankBalance) {
+			bankBalance = bankBalance - amount;
+		}else {
+			System.out.println("insufficient balance!");
+		}
+	}
+}
+
+
+class Main{
 	public static void main(String args[]) {
-		System.out.println("Hello world");
+		BankAccountSystem b = new BankAccountSystem("shiva", "normal", 21);
+		b.deposite(100);
+		b.withdraw(100);	
 	}
 }
