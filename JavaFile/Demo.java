fbc0dcab0ef16a9b8ec56d35b908bd8fe9673d86
Raghav200408 <narsapuramragava@gmail.com>
2026-06-07 23:47:29 +0530

    removed old java files

--- JavaFile/Demo.java
+++
@@ -1,9 +0,0 @@
-public class Demo {
-    public static void main(String[] args) {
-      String s1="Raghav";
-      int len = s1.length();
-      for(int i=len;i>0;i--){
-          System.out.println(s1.charAt(i));
-      }
-    }
-}
--- JavaFile/Stringreplace.java
+++
@@ -1,7 +0,0 @@
-public class Stringreplace {
-    public static void main(String[] args) {
-        String s = "Charan vamshi";
-
-        System.out.println(s.replace('a', 'o'));
-    }
-}
